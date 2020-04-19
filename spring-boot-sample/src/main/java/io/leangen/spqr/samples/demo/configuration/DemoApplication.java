a6abd8b88f64134255ff32514be8cca26c0dd3f4
ikotlyarsky <ikotlyarsky@gmail.com>
2020-04-19 10:10:26 -0400

    Update DemoApplication.java

--- spring-boot-sample/src/main/java/io/leangen/spqr/samples/demo/configuration/DemoApplication.java
+++ spring-boot-sample/src/main/java/io/leangen/spqr/samples/demo/configuration/DemoApplication.java
@@ -2,7 +2,8 @@ package io.leangen.spqr.samples.demo.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 @ComponentScan(basePackages = "io.leangen.spqr.samples.demo")
 public class DemoApplication extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(DemoApplication.class, args);
